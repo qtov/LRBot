2ff8d31306830a6a0932d418164c2f92d66d5abe
Paul Mihai FORGACI <pforgaci@bitdefender.com>
2021-06-23 12:28:27 +0300

    remove Commands categ for help

--- resources.py
+++ resources.py
@@ -8,6 +8,5 @@ tenor = TenGiphPy.Tenor(token=TENOR_TOKEN)
 intents = discord.Intents.default()
 intents.members = True
 
-help_command = commands.DefaultHelpCommand(no_category='Commands')
 bot = commands.Bot(command_prefix=PREFIX, description=DESCRIPTION,
-                   intents=intents, help_command=help_command)
+                   intents=intents)
