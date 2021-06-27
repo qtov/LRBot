152555528fa810b8d3e3381ca89cadbcd469b56f
Paul Mihai FORGACI <pforgaci@bitdefender.com>
2021-06-27 16:38:19 +0300

    add method for creating schema

--- lrbot.py
+++ lrbot.py
@@ -1,8 +1,9 @@
 import discord
+import asyncio
 from settings import (
     PREFIX, DISCORD_TOKEN, ACTIVITY_NAME, ACTIVITY_TYPE
 )
-from resources import bot
+from resources import bot, db
 from mod import Mod
 from quotes import Quotes
 try:
@@ -15,13 +16,25 @@ if uvloop is not ...:
     uvloop.install()
 
 
+async def make_db_schema():
+    async with db:
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS quotes (
+                id INT PRIMARY KEY,
+                postedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        ''')
+
+
 @bot.event
 async def on_ready():
     """Print out when LRBot is Online"""
+    db_task = asyncio.create_task(make_db_schema())
     print(f'{bot.user} is Online!')
     activity = discord.Activity(name=ACTIVITY_NAME,
                                 type=ACTIVITY_TYPE)
     await bot.change_presence(activity=activity)
+    await db_task
 
 
 @bot.event
