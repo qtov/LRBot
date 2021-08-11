06bf8e98785fb7276463d50129ba4fafaaf07020
Paul Forgaci <8023097+qtov@users.noreply.github.com>
2021-08-11 13:11:06 +0300

    Remove tenor from resources

--- resources.py
+++ resources.py
@@ -1,11 +1,7 @@
 import discord
-import TenGiphPy
 from databases import Database
 from discord.ext import commands
 from settings import PREFIX, TENOR_TOKEN, DESCRIPTION
-
-
-tenor = TenGiphPy.Tenor(token=TENOR_TOKEN)
 
 intents = discord.Intents.default()
 intents.members = True
